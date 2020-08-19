0d07aa8392b3e6cb7853d036e1936911f8868b30
henry <hnr.leto11@gmail.com>
2020-08-19 07:30:58 -0400

    rubocop sweep

--- Tic_Tac_Toe.rb
+++ Tic_Tac_Toe.rb
@@ -1,70 +1,81 @@
+# frozen_string_literal: true
+
+require 'pry'
+
+# creating of display
 class Board
-  attr_accessor :game_board
-  
+  attr_accessor :game_board, :move
   def initialize
-      @game_board=[ "", "", "", "", "", "", "", "", ""]
+    @game_board = ['', '', '', '', '', '', '', '', '']
+    @move = move
+    @error = Error.new
   end
 
   def show_board
-      puts game_board[0] + " | " + game_board[1] + "|" + game_board[2] + " "
-      puts '-+-+-'
-      puts game_board[3]+ " | " +game_board[4] + "|" + game_board[5] + " "
-      puts '-+-+-'
-      puts game_board[6] + " | " + game_board[7] + "|" + game_board[8] + " "
+    puts game_board[0] + ' | ' + game_board[1] + '|' + game_board[2] + ' '
+    puts '-+-+-'
+    puts game_board[3] + ' | ' + game_board[4] + '|' + game_board[5] + ' '
+    puts '-+-+-'
+    puts game_board[6] + ' | ' + game_board[7] + '|' + game_board[8] + ' '
   end
 end
 
+# creating of game input
 class Game
   attr_accessor :board
-
   def initialize(player1, player2)
-    @player1=player1
-    @player2=player2
+    @player1 = player1
+    @player2 = player2
     @board = Board.new
     puts "The board is seperate into nine quarants: Top-Left=0, Top-Middle=1, Top-Right=2, Middle-Left=3, Middle-Mid...etc. Standard Tic-Tac-Toe rules.\n\n\n"
   end
 
   def play_game
     board.show_board
-    player_move()
-    winner()
+    player_move
+    winner
   end
 
   def player_move
-    turn='X'
+    turn = 'X'
     9.times do
       puts "\n\n\n#{turn}, make your move"
-      move=gets.chomp.to_i
-      board.game_board[move]=turn
+      @move = gets.chomp.to_i
+
+      error.int_check(@move)
+      board.game_board[@move] = turn
       board.show_board
-      if turn=='X'
-        turn='O'
+      if turn == 'X'
+        turn = 'O'
       else
-        turn='X'
+        turn = 'X'
       end
     end
   end
 
-  def repeat_check
-
-  end
-
   def winner
-    puts "\n\n" 
+    puts "\n\n"
     board.show_board
-    puts "And the winner is..."
-      if board.game_board[0] == board.game_board[1] && board.game_board[0] == board.game_board[2] && board.game_board[0] == 'X'
-        puts "#{@player1}"
-      else if board.game_board[0] == board.game_board[1] && board.game_board[0] == board.game_board[2] && board.game_board[0] == 'O'
-        puts "#{@player2}"
-      else
-        puts "Cats game"
-      end
+    puts 'And the winner is...'
+    if board.game_board[0] == board.game_board[1] && board.game_board[0] == board.game_board[2] && board.game_board[0] == 'X'
+      puts @player1.to_s
+    elsif board.game_board[0] == board.game_board[1] && board.game_board[0] == board.game_board[2] && board.game_board[0] == 'O'
+      puts @player2.to_s
+    else
+      puts 'Cats game'
     end
   end
 end
 
+# error checking class
+class Error
+  attr_accessor :move
+  def int_check(move)
+    until move == Integer
+      gets.chomp.to_i
+    end
+  end
+end
 
-tic_tac_toe=Game.new('Henry', 'Sarah')
+tic_tac_toe = Game.new('Henry', 'Sarah')
 tic_tac_toe.play_game
-
